ea90f876a722aa882a57f9584675d932c1ae60ad
Rui Vieira <ruidevieira@googlemail.com>
2022-03-15 15:21:26 +0000

    Export results

--- notebooks/counterfactuals/utils.py
+++ notebooks/counterfactuals/utils.py
@@ -31,3 +31,36 @@ def get_negative_closest(model, probability):
     index = bad_probs.argmin()
 
     return test_x.iloc[index]
+
+def get_result_schema():
+    return ['ExternalRiskEstimate', 'MSinceOldestTradeOpen',
+       'MSinceMostRecentTradeOpen', 'AverageMInFile', 'NumSatisfactoryTrades',
+       'NumTrades60Ever2DerogPubRec', 'NumTrades90Ever2DerogPubRec',
+       'PercentTradesNeverDelq', 'MSinceMostRecentDelq',
+       'MaxDelq2PublicRecLast12M', 'MaxDelqEver', 'NumTotalTrades',
+       'NumTradesOpeninLast12M', 'PercentInstallTrades',
+       'MSinceMostRecentInqexcl7days', 'NumInqLast6M', 'NumInqLast6Mexcl7days',
+       'NetFractionRevolvingBurden', 'NetFractionInstallBurden',
+       'NumRevolvingTradesWBalance', 'NumInstallTradesWBalance',
+       'NumBank2NatlTradesWHighUtilization', 'PercentTradesWBalance',
+       'CfExternalRiskEstimate', 'CfMSinceOldestTradeOpen',
+       'CfMSinceMostRecentTradeOpen', 'CfAverageMInFile',
+       'CfNumSatisfactoryTrades', 'CfNumTrades60Ever2DerogPubRec',
+       'CfNumTrades90Ever2DerogPubRec', 'CfPercentTradesNeverDelq',
+       'CfMSinceMostRecentDelq', 'CfMaxDelq2PublicRecLast12M', 'CfMaxDelqEver',
+       'CfNumTotalTrades', 'CfNumTradesOpeninLast12M',
+       'CfPercentInstallTrades', 'CfMSinceMostRecentInqexcl7days',
+       'CfNumInqLast6M', 'CfNumInqLast6Mexcl7days',
+       'CfNetFractionRevolvingBurden', 'CfNetFractionInstallBurden',
+       'CfNumRevolvingTradesWBalance', 'CfNumInstallTradesWBalance',
+       'CfNumBank2NatlTradesWHighUtilization', 'CfPercentTradesWBalance',
+       'GoalValue', 'GoalName', 'GoalScore', 'method']
+
+def save_result(original, cf, score, method):
+    result = {}
+    result.update(original)
+    result.update(cf)
+    result.update(score)
+    result.update({"method": method})
+    result_df = pd.DataFrame.from_dict(result)
+    return result_df
